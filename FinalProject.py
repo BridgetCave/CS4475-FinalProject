05dcf6f759ed4c615890ceb503ca6321732360e9
BridgetCave <bcave3@gatech.edu>
2017-05-04 01:21:44 -0400

    Initial Commit

---
+++ FinalProject.py
@@ -0,0 +1,162 @@
+import numpy as np
+import cv2
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageStat
+
+# read in the image, create copy
+# imageName = 'hair-model-4.jpg'
+# imageName = 'shutterstock_159672665.jpg'
+imageName = 'anderne-1.jpg'
+img = cv2.imread(imageName)
+imgGrayscale = cv2.imread(imageName, 0)
+anotherImgGrayscale = cv2.imread(imageName, 0)
+
+# http://alereimondo.no-ip.org/opencv/34
+# https://github.com/shantnu/FaceDetect/blob/master/face_detect.py
+cascPath = "haarcascade_frontalface_alt.xml"
+featureCascade = cv2.CascadeClassifier(cascPath)
+
+features = featureCascade.detectMultiScale(imgGrayscale, 1.1, 5, cv2.CASCADE_SCALE_IMAGE, (100, 100))
+
+# I. Creating the Poster Edges filter from Photoshop
+# http://stackoverflow.com/questions/11064454/adobe-photoshop-style-posterization-and-opencv
+# 1. Recreate the PosterizeFilter
+n = 8   # Number of levels of quantization
+indices = np.arange(0, 256)   # List of all colors
+divider = np.linspace(0, 255, n+1)[1] # we get a divider
+quantiz = np.int0(np.linspace(0, 255, n)) # we get quantization colors
+color_levels = np.clip(np.int0(indices/divider), 0, n-1) # color levels 0,1,2..
+palette = quantiz[color_levels] # Creating the palette
+postImg = palette[img]  # Applying palette on image
+postImg = cv2.convertScaleAbs(postImg) # Converting image back to uint8
+cv2.imwrite('postImg.jpg', postImg)
+
+# II. create halftone image
+# http://stackoverflow.com/questions/10572274/halftone-images-in-python
+# 1. Convert to CMYK and split
+rbg_image = Image.open("postImg.jpg")
+cmyk_image = rbg_image.convert('CMYK')
+cmyk = cmyk_image.split()
+
+
+def gcr(im, percentage):
+    cmyk_im = im.convert('CMYK')
+    if not percentage:
+        return cmyk_im
+    cmyk_im = cmyk_im.split()
+    cmyk = []
+    for i in xrange(4):
+        cmyk.append(cmyk_im[i].load())
+    for x in xrange(im.size[0]):
+        for y in xrange(im.size[1]):
+            gray = min(cmyk[0][x,y], cmyk[1][x,y], cmyk[2][x,y]) * percentage / 100
+            for i in xrange(3):
+                cmyk[i][x,y] = cmyk[i][x,y] - gray
+            cmyk[3][x,y] = gray
+    return Image.merge('CMYK', cmyk_im)
+
+def halftone(cmyk_image, cmyk, sample):
+    cmyk = cmyk.split()
+    dots = []
+    angle = 0
+
+    for channel in cmyk:
+        # 2. Rotate each separated image by 0, 15, 30 and 45 degrees
+        channel = channel.rotate(angle, 0, 1)
+        size = channel.size[0], channel.size[1]
+        half_tone = Image.new('L', size)
+        draw = ImageDraw.Draw(half_tone)
+        # 3. Take the half-tone of each image
+        for x in xrange(0, channel.size[0], sample):
+            for y in xrange(0, channel.size[1], sample):
+                box = channel.crop((x, y, x + sample, y + sample))
+                stat = ImageStat.Stat(box)
+                diameter = (stat.mean[0] / 255) ** 0.5
+                edge = 0.5 * (1 - diameter)
+                x_pos, y_pos = (x + edge), (y + edge)
+                box_edge = sample*diameter
+                draw.ellipse((x_pos, y_pos, x_pos + box_edge, y_pos + box_edge), 255)
+        # 4. Rotate back each half-toned image
+        half_tone = half_tone.rotate(-angle, 0, 1)
+        width_half, height_half = half_tone.size
+        xx = (width_half - cmyk_image.size[0]) / 2
+        yy = (height_half - cmyk_image.size[1]) / 2
+        half_tone = half_tone.crop((xx, yy, xx + cmyk_image.size[0], yy + cmyk_image.size[1]))
+        dots.append(half_tone)
+        angle += 15
+    return dots
+
+cmyk = gcr(cmyk_image, 0)
+dots = halftone(cmyk_image, cmyk, 8)
+new = Image.merge('CMYK', dots)
+new = new.convert('RGB')
+
+# http://docs.opencv.org/2.4/doc/tutorials/imgproc/imgtrans/canny_detector/canny_detector.html
+# https://en.wikipedia.org/wiki/Canny_edge_detector
+# III. Recreate the EdgeFilter on copy
+edgeThresh = 1
+lowThreshold = 0
+max_lowThreshold = 10
+ratio = 3
+kernel_size = (3, 3)
+window_name = "Edge Map"
+
+length, width = imgGrayscale.shape
+edgeFilterImg = cv2.GaussianBlur(imgGrayscale, (7, 7), 0)
+
+
+# http://www.pyimagesearch.com/2015/04/06/zero-parameter-automatic-canny-edge-detection-with-python-and-opencv/
+sigma = 0.33
+# compute the median of the single channel pixel intensities
+v = np.median(edgeFilterImg)
+
+# apply automatic Canny edge detection using the computed median
+lower = int(max(0, (1.0 - sigma) * v))
+upper = int(min(255, (1.0 + sigma) * v))
+# lower = 100
+# upper = 150
+
+edgeFilterImg = cv2.Canny(edgeFilterImg, lower, upper)
+# edgeFilterImg = cv2.Canny(edgeFilterImg, max_lowThreshold, max_lowThreshold * ratio, (21, 21))
+
+# http://docs.opencv.org/trunk/d9/d61/tutorial_py_morphological_ops.html
+for i in range(0, 3):
+    edgeFilterImg = cv2.dilate(edgeFilterImg, (3, 3))
+
+# http://stackoverflow.com/questions/19580102/inverting-image-in-python-with-opencv
+inverseImg = (255 - edgeFilterImg)
+inverseImgColor = cv2.cvtColor(inverseImg, cv2.COLOR_GRAY2RGB)
+cv2.imwrite('inverseImgColor.jpg', inverseImgColor)
+new.save('halftoneImage.bmp')
+
+# posterEdges = cv2.addWeighted(postImg, 0.5, inverseImgColor, 0.5, 0)
+posterImg = cv2.imread('halftoneImage.bmp') * (inverseImgColor/255)
+
+# IV. Add Speech Bubble using feature detection
+posterImg = cv2.rectangle(posterImg, (0, 0), (posterImg.shape[1], posterImg.shape[0]), 0, 18)
+posterImg = cv2.rectangle(posterImg, (0, 0), (posterImg.shape[1], posterImg.shape[0]), (255, 255, 255), 8)
+
+if len(features) > 0:
+    (faceX, faceY, faceW, faceH) = features[0]
+    face_center = (faceX + faceW/2, faceY + faceH/2)
+    imageCenterX = posterImg.shape[1] / 2
+    imageCenterY = posterImg.shape[0] / 2
+    ellipseX = 0
+    ellipseY = 0
+    if face_center[0] > imageCenterX:
+        ellipseX = int(imageCenterX * 0.5)
+    else:
+        ellipseX = int(imageCenterX * 1.5)
+    if face_center[1] > imageCenterY:
+        ellipseY = int(imageCenterY * 0.5)
+    else:
+        ellipseY = int(imageCenterY * 1.5)
+    xAxis = int(imageCenterX * 0.333)
+    yAxis = int(imageCenterY * 0.333)
+    poly = cv2.ellipse2Poly((ellipseX, ellipseY), (xAxis, yAxis), 0, 0, 360, 5)
+    posterImg = cv2.fillConvexPoly(posterImg, poly, (255, 255, 255))
+    posterImg = cv2.ellipse(posterImg, ((ellipseX, ellipseY), (xAxis * 2, yAxis * 2), 0), (0, 0, 0), 4)
+    posterImg = cv2.putText(posterImg, "WOW!!", (ellipseX - 80, ellipseY + 20), cv2.FONT_HERSHEY_PLAIN, 4, (0, 0, 0), 4)
+
+cv2.imwrite('FinalImage.jpg', posterImg)
